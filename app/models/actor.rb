611b2e3be94bc8b9899cb8f805161a4e435f1a65
Yueying <wangyy395@gmail.com>
2020-03-20 11:13:22 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  #lists all of the characters that actor has alongside the show that the character is in. 
+  def list_roles
+    self.characters.map do |char|
+      "#{char.name} - #{char.show.name}"
+    end
+  end
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+    has_many :characters 
+    belongs_to :network
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.actors.map do |actor|
+            actor.full_name 
+        end
+    end
+
   
 end
